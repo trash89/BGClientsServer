687528360df866e8388c5c12029caa1d3d6b9aa4
m r <trash89@laposte.net>
2022-08-12 23:50:18 +0200

    upd

--- server.js
+++ server.js
@@ -40,12 +40,6 @@ let origin = "https://bg-clients.vercel.app";
 if (process.env.NODE_ENV !== "production") {
   origin = "http://localhost:3000";
 }
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", origin);
-  res.header("Access-Control-Allow-Credentials", true);
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
 
 app.use(
   cors({
@@ -54,6 +48,12 @@ app.use(
     methods: ["GET", "POST", "DELETE", "UPDATE", "PUT", "PATCH", "OPTIONS"],
   })
 );
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", origin);
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.use(express.json());
 app.use(helmet());

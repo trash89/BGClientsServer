33b47a5c1f97c4f13104aa3b40210909960fb193
m r <trash89@laposte.net>
2022-08-31 20:51:16 +0200

    upd

--- client/src/components/Copyright.js
+++ client/src/components/Copyright.js
@@ -1,15 +1,17 @@
 export default function Copyright() {
   return (
-    <footer className="footer">
-      {"© "} {new Date().getFullYear()}
-      {" by "}
-      <a href="https://github.com/trash89/" target="_blank" rel="noreferrer">
-        trash89
-      </a>
-      {". All rights reserved. Built with "}{" "}
-      <a href="https://reactjs.org/" target="_blank" rel="noreferrer">
-        React
-      </a>
+    <footer className="navbar navbar-expand-sm bg-black text-white navbar-dark fixed-bottom p-0 m-0">
+      <div className="d-flex flex-row flex-fill justify-content-center">
+        {"© "} {new Date().getFullYear()}
+        {" by "}
+        <a href="https://github.com/trash89/" target="_blank" rel="noreferrer">
+          trash89
+        </a>
+        {". All rights reserved. Built with "}{" "}
+        <a href="https://reactjs.org/" target="_blank" rel="noreferrer">
+          React
+        </a>
+      </div>
     </footer>
   );
 }
--- client/src/pages/Error.js
+++ client/src/pages/Error.js
@@ -1,17 +1,14 @@
 import { Link } from "react-router-dom";
 import img from "../assets/images/not-found.svg";
-import Wrapper from "../assets/wrappers/ErrorPage";
 
 const Error = () => {
   return (
-    <Wrapper className="full-page">
-      <div>
-        <img src={img} alt="not found" />
-        <h3>Ohh! Page Not Found</h3>
-        <p>We can't seem to find the page you're looking for</p>
-        <Link to="/">back home</Link>
-      </div>
-    </Wrapper>
+    <div className="d-flex flex-column justify-content-center align-content-center align-items-center">
+      <h3>Ohh! Page Not Found</h3>
+      <p>We can't seem to find the page you're looking for</p>
+      <img src={img} alt="not found" className="w-25 h-25" />
+      <Link to="/">back home</Link>
+    </div>
   );
 };
 export default Error;

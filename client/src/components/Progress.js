776640c10c203e71b2728788336212bbfba52d95
m r <trash89@laposte.net>
2022-09-07 14:24:40 +0200

    update progress

--- client/src/components/Progress.js
+++ client/src/components/Progress.js
@@ -1,8 +1,8 @@
 const Progress = () => {
   return (
     <div className="d-flex justify-content-start">
-      <p className="text-primary">Loading...</p>
-      <div className="spinner-border text-primary" />
+      <p className="text-success">Loading...</p>
+      <div className="spinner-border text-success" />
     </div>
   );
 };

d53ca2ff1b4e4664253f2b45cf59162231eebb85
m r <trash89@laposte.net>
2022-09-13 06:17:54 +0200

    update

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
     <footer className="navbar navbar-expand-sm bg-success text-white navbar-dark fixed-bottom p-0 m-0">
       <div className="d-flex flex-row flex-fill justify-content-center pe-1">
-        <small>
+        <small style={{ fontSize: "0.75rem" }}>
           {"© "} {new Date().getFullYear()}
           {" by "}
           <a href="https://github.com/trash89/" target="_blank" rel="noopener noreferrer" className="text-dark">
